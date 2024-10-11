c23bd988a69786dc7a09a26479d8d008902d7cc2
rhevo <eugenenarh1@gmail.com>
2024-10-11 00:45:32 +0000

    lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,10 +1,11 @@
 def canUnlockAll(boxes):
+    """Determine if all boxes can be opened."""
     n = len(boxes)
-    unlocked = set([0])  # Start with the first box unlocked
+    unlocked = {0}  # Start with the first box unlocked
     keys = boxes[0]  # Keys from the first box
 
     while keys:
-        key = keys.pop()  # Take a key from our list
+        key = keys.pop()  # Take a key from the list of keys
         
         # If the key opens a new box that hasn't been unlocked yet
         if key < n and key not in unlocked:
@@ -13,3 +14,4 @@ def canUnlockAll(boxes):
     
     # Check if the number of unlocked boxes equals the total number of boxes
     return len(unlocked) == n
+
